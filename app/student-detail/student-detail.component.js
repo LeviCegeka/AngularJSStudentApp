274c0409de549d4fa135952805e40409a9ca3350
LeviCegeka <levi.dellisanti@cegeka.com>
2023-05-24 23:32:44 +0200

    update

--- app/student-detail/student-detail.component.js
+++ app/student-detail/student-detail.component.js
@@ -5,11 +5,9 @@ angular.
   component('studentDetail', {
     templateUrl: 'student-detail/student-detail.template.html',
     controller: ['$routeParams', 'Student',
-      function StudentDetailController($routeParams, Student) {
+      function StudentDetailController($routeParams) {
         var self = this;
-        console.log($routeParams);
         self.student = $routeParams;
-
       }
     ]
   });
